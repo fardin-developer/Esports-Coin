c9804604ca0063712c580fd655dc1f049ab1ae0d
Chinmoy Kalita <chinmoykalita210@gmail.com>
2025-03-02 13:30:45 +0530

    added origin all

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3002'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
